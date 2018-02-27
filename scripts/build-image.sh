98a9102cc90908f1465f6cd7f00df8b6e1593218
kyamazawa <yamazawa@supersoftware.co.jp>
2018-02-27 10:19:01 +0900

    change binary link

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -7,8 +7,8 @@ VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
 CNI_VERSION="v0.6.0"
 
 mkdir -p dist
-$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
-$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
+$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-arm64-$CNI_VERSION.tgz | tar -xz -C dist/
+$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-arm64-$CNI_VERSION.tgz | tar -xz -C dist/
 
 docker build --no-cache -t $IMAGE_NAME:$VERSION .
 docker push $IMAGE_NAME:$VERSION
